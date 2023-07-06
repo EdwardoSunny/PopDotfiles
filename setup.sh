7544e8a92e6f00549df705af461704ad0eb5b408
Edward Sun <edward.sun2015@gmail.com>
2023-07-05 23:30:43 -0700

    init commit with set up file

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dotfiles="$HOME/dotfiles"
+
+echo "Updating Packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Dependencies (Pop_OS/Ubuntu ONLY)"
+sudo apt-get install i3 rofi neofetch git fish polybar feh kitty python3 polybar emacs neovim
+
+ln -s $HOME/PopDotfiles/i3 ~/.config
+ln -s $HOME/PopDotfiles/fish ~/.config
+ln -s $HOME/PopDotfiles/polybar ~/.config
+ln -s $HOME/PopDotfiles/rofi ~/.config
+ln -s $HOME/PopDotfiles/kitty ~/.config
+ln -s $HOME/PopDotfiles/neofetch ~/.config
