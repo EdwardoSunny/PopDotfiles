9a0ab9be6bdc155e8c60b4381538b82d917098a4
Edward Sun <edward.sun2015@gmail.com>
2023-07-05 23:44:51 -0700

    added picture

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo "Updating Packages..."
 sudo apt update && sudo apt upgrade -y
 
 echo "Installing Dependencies (Pop_OS/Ubuntu ONLY)"
-sudo apt-get install i3 rofi neofetch git fish polybar feh kitty python3 polybar emacs neovim
+sudo apt-get install i3 rofi neofetch git fish polybar feh kitty python3 polybar emacs neovim compton compton-conf
 
 ln -s $HOME/PopDotfiles/i3 ~/.config
 ln -s $HOME/PopDotfiles/fish ~/.config
